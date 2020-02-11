e3c6db44b484bfd20f9149347102bd8466d154a5
Kish29 <875691208@qq.com>
2020-02-11 18:54:41 +0800

    Feb.11

--- KeepLearning/KeepLearning.cpp
+++ KeepLearning/KeepLearning.cpp
@@ -36,3 +36,85 @@ int main() {
 	std::cout << "Hello World!\n";
 	std::cout << INT_MIN;
 }
+
+
+
+#include<iostream>
+#include<climits>
+#include<Windows.h>
+#include<string>
+
+static class Inereat {
+public:
+	Inereat() {};
+	static void shows();
+};
+
+void Inereat::shows() {
+	std::cout << "This is the static class's function." << std::endl;
+}
+
+
+void funtcion() {
+	std::cout << "This is the function call" << std::endl;
+}
+
+
+class Make {
+public:
+	Make() {};
+	//Use string
+	std::string name;
+	long long id_64;
+	void initial(std::string name, long long id_64);
+private:
+
+};
+
+int main() {
+	int s = rand();
+	funtcion();
+	std::cout << "Hello World!\n";
+	std::cout << INT_MIN;
+}
+
+
+
+#include<iostream>
+#include<climits>
+#include<Windows.h>
+#include<string>
+
+static class Inereat {
+public:
+	Inereat() {};
+	static void shows();
+};
+
+void Inereat::shows() {
+	std::cout << "This is the static class's function." << std::endl;
+}
+
+
+void funtcion() {
+	std::cout << "This is the function call" << std::endl;
+}
+
+
+class Make {
+public:
+	Make() {};
+	//Use string
+	std::string name;
+	long long id_64;
+	void initial(std::string name, long long id_64);
+private:
+
+};
+
+int main() {
+	int s = rand();
+	funtcion();
+	std::cout << "Hello World!\n";
+	std::cout << INT_MIN;
+}
